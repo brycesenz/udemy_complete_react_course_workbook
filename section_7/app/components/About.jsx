cc343073c3883eb4e6d202bd07df99615404941b
Bryce Senz <bryce.senz@gmail.com>
2017-06-04 22:55:12 -0400

    Adding basic custom styles

--- section_7/app/components/About.jsx
+++ section_7/app/components/About.jsx
@@ -4,7 +4,7 @@ var About = (props) => {
   return (
     <div>
       <div>
-        <h1 className='text-center'>About this app</h1>
+        <h1 className='text-center page-title'>About this app</h1>
         <p>This is just a demo app for learning more about building React.js apps</p>
       </div>
     </div>
--- section_7/app/components/WeatherForm.jsx
+++ section_7/app/components/WeatherForm.jsx
@@ -14,7 +14,7 @@ var WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.submitHandler}>
-          <input type='text' ref='location' placeholder='Enter location'></input>
+          <input type='search' ref='location' placeholder='Search weather by city'></input>
           <button className='button expanded hollow'>Get Weather</button>
         </form>
       </div>
