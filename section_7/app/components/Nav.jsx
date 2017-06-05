deab91855a48d15730c7025df32756886c61a5c7
Bryce Senz <bryce.senz@gmail.com>
2017-06-05 18:40:13 -0400

    Wiring up new header search

--- section_7/app/components/Nav.jsx
+++ section_7/app/components/Nav.jsx
@@ -4,7 +4,16 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(evt) {
     evt.preventDefault();
-    alert('Not yet wired up.');
+
+    var location = this.refs.location.value;
+
+    //Now go to our URL with that query string
+    var encodedLocation = encodeURIComponent(location);
+
+    if (location.length > 0) {
+      this.refs.location.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
   },
   render: function() {
     return (
@@ -28,7 +37,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className='menu'>
               <li>
-                <input type='search' placeholder='Search weather by city'></input>
+                <input type='search' ref='location' placeholder='Search weather by city'></input>
               </li>
               <li>
                 <input type='submit' className='button' value='Get Weather'></input>
--- section_7/app/components/Weather.jsx
+++ section_7/app/components/Weather.jsx
@@ -11,10 +11,28 @@ var Weather = React.createClass({
       errorMessage: undefined
     }
   },
+  componentDidMount: function() {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
   handleSearch: function(location) { 
     var that = this;
 
     this.setState({
+      location: undefined,
+      temp: undefined,
       isLoading: true,
       errorMessage: undefined
     });
