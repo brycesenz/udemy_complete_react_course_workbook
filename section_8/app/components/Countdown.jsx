6c231da22089a9e509f944c458464fb28637dd56
Bryce Senz <bryce.senz@gmail.com>
2017-06-23 00:04:27 -0400

    Adding in CountdownForm

--- section_8/app/components/Countdown.jsx
+++ section_8/app/components/Countdown.jsx
@@ -1,12 +1,24 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+  getInitialState: function() {
+    return {
+      count: 0
+    }
+  },
+  handleCountdown: function(seconds) {
+    this.setState({count: seconds});
+  },
   render: function() {
+    var {count} = this.state;
+
     return (
       <div className='countdown'>
         <p>Countdown!</p>
-        <Clock totalSeconds={129}/>
+        <Clock totalSeconds={count}/>
+        <CountdownForm onSetCountdown={this.handleCountdown}/>
       </div>
     );    
   }
---
+++ section_8/app/tests/components/CountdownForm.test.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+var CountdownForm = require('CountdownForm');
+
+// describe('CountdownForm', () => {
+//   it('should exist', () => {
+//     expect(Clock).toExist();
+//   });
+
+//   describe('render', () => {
+//     it('should render clock to output', () => {
+//       var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+//       var $el = $(ReactDOM.findDOMNode(clock));
+//       var actualText = $el.find('.clock-text').text();
+//       expect(actualText).toEqual('01:02')
+//     });
+//   });
+
+//   describe('formatSeconds', () => {
+//     it('should format seconds with large remainder', () => {
+//       var clock = TestUtils.renderIntoDocument(<Clock/>);
+//       var seconds = 615;
+//       expect(clock.formatSeconds(seconds)).toEqual('10:15');
+//     });
+
+//     it('should format seconds with small remainder', () => {
+//       var clock = TestUtils.renderIntoDocument(<Clock/>);
+//       var seconds = 605;
+//       expect(clock.formatSeconds(seconds)).toEqual('10:05');
+//     });
+
+//     it('should format seconds with small minutes', () => {
+//       var clock = TestUtils.renderIntoDocument(<Clock/>);
+//       var seconds = 61;
+//       expect(clock.formatSeconds(seconds)).toEqual('01:01');
+//     });
+//   });
+// });
+
