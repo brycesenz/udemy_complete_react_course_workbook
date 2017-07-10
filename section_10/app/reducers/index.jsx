18b8e192acab60b8502293951fa06fc26dfc4d75
Bryce Senz <bryce.senz@gmail.com>
2017-07-10 00:50:36 -0400

    Passing reducer specs

--- section_10/app/reducers/index.jsx
+++ section_10/app/reducers/index.jsx
@@ -7,6 +7,15 @@ export var searchTextReducer = (state = '', action) => {
   }
 }
 
+export var showCompletedReducer = (state = false, action) => {
+  switch (action.type) {
+    case 'TOGGLE_SHOW_COMPLETED':
+      return !state;
+    default:
+      return state;
+  }
+}
+
 var nextTodoId = 1;
 export var todoReducer = (state = [], action) => {
   switch (action.type) {
--- section_10/app/tests/reducers/index.test.jsx
+++ section_10/app/tests/reducers/index.test.jsx
@@ -1,12 +1,25 @@
 var expect = require('expect');
+
+// Wrapping things in df 'freezes' them,
+//  so that if they are altered in any way by the function
+//  that they go into, the tests will fail.  This ensures idempotence.
+var df = require('deep-freeze-strict');
 var reducers = require('reducers');
 
 describe('Reducers', () => {
-  it('should generate search text action', () => {
+  it('should handle CHANGE_SEARCH_TEXT action', () => {
     var action = {
       type: 'CHANGE_SEARCH_TEXT',
       searchText: 'my dummy search'
     };
-    expect(reducers.searchTextReducer('', action)).toEqual('my dummy search');
+    expect(reducers.searchTextReducer(df(''), df(action))).toEqual('my dummy search');
+  });
+
+  it('should flip show completed status', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    expect(reducers.showCompletedReducer(false, action)).toEqual(true);
+    expect(reducers.showCompletedReducer(true, action)).toEqual(false);
   });
 });
